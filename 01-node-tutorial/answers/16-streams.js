921a9a9837d9b1df686eaee3962ee2c7a136646d
Vitalii Popovych <popovych@gmail.com>
2023-01-17 23:15:19 -0600

    answers for lesson 2

---
+++ 01-node-tutorial/answers/16-streams.js
@@ -0,0 +1,10 @@
+const { createReadStream } = require('fs');
+
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+const stream = createReadStream('./content/big.txt');
+
+stream.on('data', (result) => {
+  console.log(result);
+});
+stream.on('error', (err) => console.log(err));
